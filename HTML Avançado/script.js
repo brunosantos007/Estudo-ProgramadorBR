743bbdb08f7ceb33de64b1eee8ca5ed245df09b0
Bruno Santos <brunogsantoss@outlook.com>
2022-02-23 16:09:43 -0300

    Player do Spotify

--- HTML Avançado/script.js
+++ HTML Avançado/script.js
@@ -1,16 +1,37 @@
+let cor = document.getElementById("color")
 let audio = document.getElementById("music")
 let btnPlay = document.getElementById("btn-play")
 let btnPause = document.getElementById("btn-pause")
+let img = document.getElementById("img")
+let musica = document.getElementById("music")
+let back = document.getElementById("back")
+let next = document.getElementById("next")
+
+btnPause.style.display = "none"
+
 
 
 function play(){
     audio.play()
-    play.style.display = "none"
-    pause.style.display = "block"
+    btnPlay.setAttribute("id","btn-pause")
+    btnPause.style.display = "inline-block"
+    btnPlay.style.display = "none"
 }
 function pause(){
     audio.pause()
-    pause.style.display = "nonne"
-    pause.style.display = "block"
+    btnPause.setAttribute("id","btn-play")
+    btnPause.style.display = "none"
+    btnPlay.style.display = "inline-block"
+}
+function musicaAnterior(){
+    for(let i = 0; i < back.value;i--){
+        return 0
+    }
 }
 
+function proximaMusica(){
+    pause()
+    img.setAttribute("src","imagens/alleyes on me.png")
+    musica.setAttribute("src","musicas/2Pac - All Eyez On Me (128 kbps).mp3")
+    //body.style.backgroundColor = rgba(0, 0, 0, 0.822)
+}
